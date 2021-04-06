712921b3f0c990e20cc9959c058af887c2b03629
grits <grits.0123@gmail.com>
2021-04-06 21:33:11 +0300

    Replace LocalDate to Date.

--- backend/core/src/main/java/com/epolsoft/domain/hero/Hero.java
+++ backend/core/src/main/java/com/epolsoft/domain/hero/Hero.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
@@ -23,7 +24,7 @@ public class Hero {
 
     private Boolean isMelee;
 
-    private Date dateOfCreated;
+    private LocalDate dateOfCreated;
 
     private Set<Spell> spells;
 
