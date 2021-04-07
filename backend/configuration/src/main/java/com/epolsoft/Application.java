feafdf666fd3efabe87de9f5a02e4ec523572dc5
grits <grits.0123@gmail.com>
2021-04-07 14:48:59 +0300

    Scan packages for project. Init of time (UTC).

--- backend/configuration/src/main/java/com/epolsoft/Application.java
+++ backend/configuration/src/main/java/com/epolsoft/Application.java
@@ -1,12 +1,28 @@
 package com.epolsoft;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication(scanBasePackages = "com.epolsoft.controller")
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
+@SpringBootApplication(scanBasePackages = {"com.epolsoft.controller","com.epolsoft.mapper",
+        "com.epolsoft.repo", "com.epolsoft.port"})
 public class Application extends SpringBootServletInitializer {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
 }
