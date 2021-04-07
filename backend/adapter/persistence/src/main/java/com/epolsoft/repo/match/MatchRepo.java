f311ce69ab6ac1b65f479dd5794a694443544f51
grits <grits.0123@gmail.com>
2021-04-07 14:47:00 +0300

    MatchJpa repository.

--- backend/adapter/persistence/src/main/java/com/epolsoft/repo/match/MatchRepo.java
+++ backend/adapter/persistence/src/main/java/com/epolsoft/repo/match/MatchRepo.java
@@ -1,9 +1,10 @@
-package com.epolsoft.repo;
+package com.epolsoft.repo.match;
 
+import com.epolsoft.dto.match.MatchJpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepo extends JpaRepository<MessageJpa, Long>, JpaSpecificationExecutor<MessageJpa> {
+public interface MatchRepo extends JpaRepository<MatchJpa, Long>, JpaSpecificationExecutor<MatchJpa> {
 }
