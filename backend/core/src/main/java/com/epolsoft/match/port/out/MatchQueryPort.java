5ee154f0576b1ec6381442af4cd3ebbd5d62ee35
grits <grits.0123@gmail.com>
2021-04-08 23:23:02 +0300

    Port for Match.

---
+++ backend/core/src/main/java/com/epolsoft/match/port/out/MatchQueryPort.java
@@ -0,0 +1,40 @@
+package com.epolsoft.match.port.out;
+
+
+import com.epolsoft.match.domain.Match;
+import com.epolsoft.match.domain.Region;
+import com.epolsoft.match.domain.TypeOfMatch;
+import lombok.Data;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+
+public interface MatchQueryPort {
+
+    Match findMatchById(Long id) throws Exception;
+
+    void deleteMatch(Long id) throws Exception;
+
+    Match saveNewMatch(Match match) throws Exception;
+
+    Match updateMatch(Long id, Match match) throws Exception;
+
+    Page<Match> findPageOfMatch(MatchFiltered matchFiltered, Pageable pageable) throws Exception;
+
+    List<Match> findAll() throws Exception;
+
+
+    @Data
+    class MatchFiltered {
+
+        private Long id;
+
+        private TypeOfMatch type;
+
+        private Region region;
+
+    }
+
+}
