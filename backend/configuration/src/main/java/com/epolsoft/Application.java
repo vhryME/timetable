3f6211db73b07429a6523e0caee6b676b7025c72
grits <grits.0123@gmail.com>
2021-04-08 17:40:56 +0300

    Init of Timezone. Updated of application.yml.

--- backend/configuration/src/main/java/com/epolsoft/Application.java
+++ backend/configuration/src/main/java/com/epolsoft/Application.java
@@ -3,19 +3,14 @@ package com.epolsoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 
-@SpringBootApplication(scanBasePackages = {"com.epolsoft.controller","com.epolsoft.mapper",
-        "com.epolsoft.repo", "com.epolsoft.port"})
+@SpringBootApplication(scanBasePackages = "com.epolsoft")
 public class Application extends SpringBootServletInitializer {
-
     @PostConstruct
     public void init() {
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
@@ -24,5 +19,4 @@ public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
