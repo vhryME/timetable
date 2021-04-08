b13cf88a11db0be039f30fb3a821db74fe5b9e44
grits <grits.0123@gmail.com>
2021-04-08 17:40:15 +0300

    Demo version of MatchController.

---
+++ backend/adapter/web/src/main/java/com/epolsoft/match/controller/MatchController.java
@@ -0,0 +1,44 @@
+package com.epolsoft.match.controller;
+
+import com.epolsoft.match.domain.Match;
+import com.epolsoft.match.port.in.MatchUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/match")
+@RequiredArgsConstructor
+public class MatchController {
+
+    private final MatchUseCase useCase;
+
+
+    @PostMapping("/new")
+    public Match saveNewMatch() {
+        return useCase.saveNewMatch(new Match());
+    }
+
+    @GetMapping("/get/{id}")
+    public Match getMatch(@PathVariable("id") Long id) {
+        return useCase.getMatch(id);
+    }
+
+    @GetMapping("/all")
+    public List<Match> getAllMatches() {
+        return useCase.findAll();
+    }
+
+    @GetMapping("/get_page")
+    public Page<Match> getAllPages() {
+        return useCase.findPageOfMatch();
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteMatch(@PathVariable("id") Long id) {
+        useCase.deleteMatch(id);
+    }
+
+}
