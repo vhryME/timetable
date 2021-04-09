07324eeae2c38d3b909ec00f508e63c3fedf6b4c
grits <grits.0123@gmail.com>
2021-04-09 10:57:51 +0300

    Pagination end-point.

--- backend/adapter/web/src/main/java/com/epolsoft/match/controller/MatchController.java
+++ backend/adapter/web/src/main/java/com/epolsoft/match/controller/MatchController.java
@@ -3,8 +3,8 @@ package com.epolsoft.match.controller;
 import com.epolsoft.match.domain.Match;
 import com.epolsoft.match.port.in.MatchUseCase;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,25 +16,35 @@ public class MatchController {
     private final MatchUseCase useCase;
 
 
+    @GetMapping("/init")
+    public void init() {
+        useCase.init();
+    }
+
+
     @PostMapping("/new")
     public Match saveNewMatch() {
         return useCase.saveNewMatch(new Match());
     }
+
 
     @GetMapping("/get/{id}")
     public Match getMatch(@PathVariable("id") Long id) {
         return useCase.getMatch(id);
     }
 
+
     @GetMapping("/all")
     public List<Match> getAllMatches() {
         return useCase.findAll();
     }
 
-    @GetMapping("/get_page")
-    public Page<Match> getAllPages() {
-        return useCase.findPageOfMatch();
+
+    @GetMapping("/get_all_pages/{size}/{num}")
+    public Page<Match> getAllPages(@PathVariable("size") Integer pageSize, @PathVariable("num") Integer pageNumber) {
+        return useCase.findAllPages(pageSize, pageNumber);
     }
+
 
     @DeleteMapping("/delete/{id}")
     public void deleteMatch(@PathVariable("id") Long id) {
