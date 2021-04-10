5b5cbc2e5f9f48e3921fd941308678dfb380e35b
grits <grits.0123@gmail.com>
2021-04-10 12:42:12 +0300

    CRUD api for Match class.

--- backend/adapter/web/src/main/java/com/epolsoft/match/controller/MatchController.java
+++ backend/adapter/web/src/main/java/com/epolsoft/match/controller/MatchController.java
@@ -2,9 +2,11 @@ package com.epolsoft.match.controller;
 
 import com.epolsoft.match.domain.Match;
 import com.epolsoft.match.port.in.MatchUseCase;
+import com.epolsoft.match.port.out.MatchQueryPort;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,15 +25,27 @@ public class MatchController {
     }
 
 
-    @PostMapping("/new")
-    public Match saveNewMatch() {
-        return useCase.saveNewMatch(new Match());
+    @DeleteMapping("/delete/{id}")
+    public void deleteMatch(@PathVariable("id") Long id) {
+        useCase.deleteMatch(id);
     }
 
 
     @GetMapping("/get/{id}")
     public Match getMatch(@PathVariable("id") Long id) {
         return useCase.getMatch(id);
+    }
+
+
+    @PostMapping(value = "/new", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Match saveNewMatch(@RequestBody Match match) {
+        return useCase.saveNewMatch(match);
+    }
+
+
+    @PutMapping(value = "/edit/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Match updateMatch(@PathVariable("id") Long id, @RequestBody Match match) {
+        return useCase.updateMatch(id, match);
     }
 
 
@@ -42,14 +56,14 @@ public class MatchController {
 
 
     @GetMapping("/get_pages")
-    public Page<Match> getAllPages(Pageable page) {
-        return useCase.findAllPages(page);
+    public Page<Match> getPages(Pageable page) {
+        return useCase.findPageOfMatch(page);
     }
 
 
-    @DeleteMapping("/delete/{id}")
-    public void deleteMatch(@PathVariable("id") Long id) {
-        useCase.deleteMatch(id);
+    @GetMapping("/get_filtered_pages")
+    public Page<Match> getFilteredPages(Pageable pageable ,MatchQueryPort.MatchFiltered matchFiltered) {
+        return useCase.findPageOfMatchFiltered(pageable, matchFiltered);
     }
 
 }
