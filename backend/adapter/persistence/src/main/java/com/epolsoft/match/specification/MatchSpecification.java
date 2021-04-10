53c43ffc3282ae4883cb87f05ea65ff3538881b7
grits <grits.0123@gmail.com>
2021-04-10 12:41:28 +0300

    Checking on null in specification

--- backend/adapter/persistence/src/main/java/com/epolsoft/match/specification/MatchSpecification.java
+++ backend/adapter/persistence/src/main/java/com/epolsoft/match/specification/MatchSpecification.java
@@ -1,24 +1,21 @@
 package com.epolsoft.match.specification;
 
-import com.epolsoft.match.domain.Region;
-import com.epolsoft.match.domain.TypeOfMatch;
+
 import com.epolsoft.match.dto.MatchJpa;
+import com.epolsoft.match.dto.RegionJpa;
+import com.epolsoft.match.dto.TypeOfMatchJpa;
 import org.springframework.data.jpa.domain.Specification;
+
 
 public class MatchSpecification {
 
-    public static Specification<MatchJpa> findById(Long id) {
-        return (((root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("id"), id)));
+    public static Specification<MatchJpa> findByType(TypeOfMatchJpa type) {
+        return (root, criteriaQuery, criteriaBuilder) -> type == null ? null : criteriaBuilder.equal(root.get("type"), type);
     }
 
 
-    public static Specification<MatchJpa> findByType(TypeOfMatch type) {
-        return (((root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("type"), type)));
-    }
-
-
-    public static Specification<MatchJpa> findByRegion(Region region) {
-        return (((root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(root.get("region"), region)));
+    public static Specification<MatchJpa> findByRegion(RegionJpa region) {
+        return (root, criteriaQuery, criteriaBuilder) -> region == null ? null : criteriaBuilder.equal(root.get("region"), region);
     }
 
 }
