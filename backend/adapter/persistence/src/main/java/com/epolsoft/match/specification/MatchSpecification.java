165912b60f2a3ffe9dad01b89410f1a58cc92a62
grits <grits.0123@gmail.com>
2021-04-12 20:03:32 +0300

    Added more specification.

--- backend/adapter/persistence/src/main/java/com/epolsoft/match/specification/MatchSpecification.java
+++ backend/adapter/persistence/src/main/java/com/epolsoft/match/specification/MatchSpecification.java
@@ -1,10 +1,14 @@
 package com.epolsoft.match.specification;
 
 
-import com.epolsoft.match.dto.MatchJpa;
-import com.epolsoft.match.dto.RegionJpa;
-import com.epolsoft.match.dto.TypeOfMatchJpa;
+import com.epolsoft.match.jpa.MapJpa;
+import com.epolsoft.match.jpa.MatchJpa;
+import com.epolsoft.match.jpa.RegionJpa;
+import com.epolsoft.match.jpa.TypeOfMatchJpa;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 
 public class MatchSpecification {
@@ -14,8 +18,23 @@ public class MatchSpecification {
     }
 
 
+    public static Specification<MatchJpa> findByDate(LocalDate date) {
+        return (root, criteriaQuery, criteriaBuilder) -> date == null ? null : criteriaBuilder.equal(root.get("date"), date);
+    }
+
+
     public static Specification<MatchJpa> findByRegion(RegionJpa region) {
         return (root, criteriaQuery, criteriaBuilder) -> region == null ? null : criteriaBuilder.equal(root.get("region"), region);
+    }
+
+
+    public static Specification<MatchJpa> findByDuration(Double duration) {
+        return (root, criteriaQuery, criteriaBuilder) -> duration == null ? null : criteriaBuilder.equal(root.get("duration"), duration);
+    }
+
+
+    public static Specification<MatchJpa> findByMaps(Set<MapJpa> maps) {
+        return (root, criteriaQuery, criteriaBuilder) -> maps == null ? null : criteriaBuilder.equal(root.get("maps"), maps);
     }
 
 }
