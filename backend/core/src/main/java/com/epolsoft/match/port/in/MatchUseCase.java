d4fb1029ad138c743bc77d90948ea6ecca4ce1ac
grits <grits.0123@gmail.com>
2021-04-12 20:05:26 +0300

    Update of id type for InMemory.

--- backend/core/src/main/java/com/epolsoft/match/port/in/MatchUseCase.java
+++ backend/core/src/main/java/com/epolsoft/match/port/in/MatchUseCase.java
@@ -2,7 +2,7 @@ package com.epolsoft.match.port.in;
 
 
 import com.epolsoft.match.domain.Match;
-import com.epolsoft.match.port.out.MatchQueryPort;
+import com.epolsoft.match.port.out.MatchPort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,20 +11,18 @@ import java.util.List;
 
 public interface MatchUseCase {
 
-    void init();
+    Match getMatch(Integer id);
 
-    Match getMatch(Long id);
-
-    void deleteMatch(Long id);
+    void deleteMatch(Integer id);
 
     Match saveNewMatch(Match match);
 
-    Match updateMatch(Long id, Match match);
+    Match updateMatch(Integer id, Match match);
 
     List<Match> findAll();
 
     Page<Match> findPageOfMatch(Pageable pageable);
 
-    Page<Match> findPageOfMatchFiltered(Pageable pageable, MatchQueryPort.MatchFiltered matchFiltered);
+    Page<Match> findPageOfMatchFiltered(Pageable pageable, MatchPort.MatchFiltered matchFiltered);
 
 }
