fd3a937f15b2e3bc2fbf054d55f15dd0c718fce4
grits <grits.0123@gmail.com>
2021-04-14 14:43:46 +0300

    Inserted ids to Map enum.

--- backend/core/src/main/java/com/epolsoft/match/domain/Map.java
+++ backend/core/src/main/java/com/epolsoft/match/domain/Map.java
@@ -1,7 +1,25 @@
 package com.epolsoft.match.domain;
 
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.Arrays;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
 public enum Map {
     AlteracPass, BattleFieldOfEternity, BlackheartsBay, BraxisOutpost, CursedHollow, DragonShire, GardenOfTerror,
     HanamuraTemple, HauntedMines, InfernalShines, LostCavern, SilverCity, SkyTemple, TombOfTheSpiderQueen,
-    TowersOfDoom, VolskayaFoundry, WarheadJunction, Unknown
+    TowersOfDoom, VolskayaFoundry, WarheadJunction, Unknown;
+
+    public Integer id;
+
+    public static Map getStateById(Integer stateId) {
+        return Arrays.stream(Map.values())
+                .filter(map -> map.id.equals(stateId))
+                .findFirst()
+                .orElse(null);
+    }
 }
