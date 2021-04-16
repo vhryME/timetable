590e7176fc1df28a3292e84a32ca0bc114bc1703
grits <grits.0123@gmail.com>
2021-04-16 11:00:03 +0300

    Mapper for inputting spells.

--- backend/adapter/web/src/main/java/com/epolsoft/hero/mapper/SpellDtoMapper.java
+++ backend/adapter/web/src/main/java/com/epolsoft/hero/mapper/SpellDtoMapper.java
@@ -1,10 +1,12 @@
 package com.epolsoft.hero.mapper;
 
 
+import com.epolsoft.hero.domain.Spell;
+import com.epolsoft.hero.dto.in.SpellDtoIn;
 import org.mapstruct.Mapper;
 
 
 @Mapper
 public interface SpellDtoMapper {
-
+    Spell SpellDtoInToSpell(SpellDtoIn spellDtoIn);
 }
